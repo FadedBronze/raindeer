2008a3eddabf6b83ef22e2275d57ae842f338ffd
FadedBronze <TheSamyatGautam@gmail.com>
2024-10-07 20:59:09 -0400

    intersect vector lines function

--- src/triangulate.rs
+++ src/triangulate.rs
@@ -1,4 +1,41 @@
-use cgmath::Vector2;
+use cgmath::{Vector2, Zero};
+
+use crate::path_builder::RDStroke;
+
+fn intersect_lines(a: Vector2<f32>, b: Vector2<f32>, c: Vector2<f32>, d: Vector2<f32>) -> Option<Vector2<f32>> {
+    let slope_ab = (b.y - a.y) / (b.x - a.x);
+    let slope_cd = (d.y - c.y) / (d.x - c.x);
+
+    if slope_ab == slope_cd {
+        return None;
+    }
+    
+    let intercept_ab = a.y - slope_ab * a.x;
+    let intercept_cd = c.y - slope_cd * c.x;    
+    
+    if slope_ab == f32::INFINITY {
+        let y = slope_cd * a.x + intercept_cd;
+        return Some(Vector2::new(a.x, y));
+    }
+    
+    if slope_cd == f32::INFINITY {
+        let y = slope_ab * c.x + intercept_ab;
+        return Some(Vector2::new(c.x, y));
+    }
+
+    let x = (intercept_cd - intercept_ab) / (slope_ab - slope_cd);
+    let y = slope_ab * x + intercept_ab;
+
+    Some(Vector2::new(x, y))
+}
+
+pub(crate) fn triangulate_stroke(vertices: &[Vector2<f32>], stroke: &RDStroke) -> (Vec<Vector2<f32>>, Vec<u32>) {
+    let mut vertices = vec![];
+    let mut indicies = vec![];
+
+
+    (vertices, indicies)
+}
 
 //1. no colinear edges
 //2. no clockwise verticies
@@ -50,6 +87,15 @@ pub(crate) fn within_triangle(a: Vector2<f32>, b: Vector2<f32>, c: Vector2<f32>,
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_intersect_lines() {
+        assert_eq!(intersect_lines(Vector2::new(5.0, 5.0), Vector2::new(-5.0, 5.0), Vector2::new(0.0, 0.0), Vector2::new(0.0, 10.0)), Some(Vector2::new(0.0, 5.0)));
+        assert_eq!(intersect_lines(Vector2::new(0.0, 0.0), Vector2::new(5.0, 5.0), Vector2::new(10.0, 0.0), Vector2::new(10.0, 20.0)), Some(Vector2::new(10.0, 10.0)));
+        assert_eq!(intersect_lines(Vector2::new(0.0, 0.0), Vector2::new(2.5, 2.5), Vector2::new(10.0, 0.0), Vector2::new(7.5, 2.5)), Some(Vector2::new(5.0, 5.0)));
+        assert_eq!(intersect_lines(Vector2::new(0.0, 0.0), Vector2::new(5.0, 5.0), Vector2::new(10.0, 0.0), Vector2::new(5.0, 5.0)), Some(Vector2::new(5.0, 5.0)));
+        assert_eq!(intersect_lines(Vector2::new(0.0, 0.0), Vector2::new(0.0, 5.0), Vector2::new(10.0, 0.0), Vector2::new(10.0, 5.0)), None);
+    }
 
     #[test]
     fn test_within_triangle() {
